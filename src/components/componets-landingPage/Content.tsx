51c27dc81dbde1096e0ce7090dc274db3949688e
Henrique Batista <wellington.h.batista@hotmail.com>
2022-08-28 17:45:00 -0300

    correction link Linkedin

--- src/components/componets-landingPage/Content.tsx
+++ src/components/componets-landingPage/Content.tsx
@@ -11,7 +11,7 @@ export function Content() {
               Click the button to view my resume on Linkedin
             </p>
           </div>
-          <a href="https://www.linkedin.com/in/wellington-henrique-batista-55a83556/recent-activity/shares/" className="inline-flex items-center justify-center w-full px-5 py-4 mt-6 ml-0 font-sans text-base leading-none text-white no-underline bg-indigo-600 border border-indigo-600 border-solid rounded cursor-pointer md:w-auto lg:mt-0 hover:bg-indigo-700 hover:border-indigo-700 hover:text-white focus-within:bg-indigo-700 focus-within:border-indigo-700 focus-within:text-white sm:text-lg lg:ml-6 md:text-xl" data-primary="indigo-600" data-rounded="rounded">
+          <a href="https://www.linkedin.com/in/wellington-henrique-batista-55a83556/" className="inline-flex items-center justify-center w-full px-5 py-4 mt-6 ml-0 font-sans text-base leading-none text-white no-underline bg-indigo-600 border border-indigo-600 border-solid rounded cursor-pointer md:w-auto lg:mt-0 hover:bg-indigo-700 hover:border-indigo-700 hover:text-white focus-within:bg-indigo-700 focus-within:border-indigo-700 focus-within:text-white sm:text-lg lg:ml-6 md:text-xl" data-primary="indigo-600" data-rounded="rounded">
             View
             <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-2" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
               <line x1="5" y1="12" x2="19" y2="12"></line>
